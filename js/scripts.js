3736e2d1b6d3f4cbf642f593547d9cd4a210e21f
Anna Becker <anna.r.becker@gmail.com>
2016-01-22 09:21:46 -0800

    setup index page with form for user to enter number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,5 @@
-// function countUp(number) {
-//   var numberArray = [];
-//   for (var i = 1; i <= number; i++) {
-//     numberArray.push(i);
-//   }
-//   return numberArray;
-// };
-//
-// var pingPong = function(number) {
-//   if (number % 3 === 0 && number % 5 === 0) {
-//     return "pingpong";
-//   }
-//   else if (number % 5 === 0) {
-//     return "pong";
-//   }
-//   else if (number % 3 === 0) {
-//     return "ping";
-//   }
-//   else if (number % 3 !== 0 || number % 5 !== 0 || number % 15 !== 0 ) {
-//     return number;
-//   }
-// };
-
 var pingPong = function (number) {
   var numberArray = [];
-
   for (var i = 1; i <= number; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       numberArray.push("pingpong");
@@ -40,3 +16,7 @@ var pingPong = function (number) {
   }
   return numberArray;
 };
+
+// $(document).ready(function() {
+//
+// });
