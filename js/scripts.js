39db9eb8dc95b048b3325f68c5e8bdd1ee065dc8
Anna Becker <anna.r.becker@gmail.com>
2016-01-22 08:17:51 -0800

    add functionality to count up to user's number by 1.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function countUp(number) {
+  var numberArray = [];
+  for (var i = 1; i <= number; i++) {
+    numberArray.push(i);
+  }
+  return numberArray;
+};
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe("countUp", function(){
+  it("will count up to a user's number by 1 starting at 1 and put the numbers into an array", function(){
+    expect(countUp(2)).to.eql([1,2]);
+  });
+});
