d8933ebcabbfcc8b02924946d2d6e38f6690c8cc
Anna Becker <anna.r.becker@gmail.com>
2016-01-22 08:41:08 -0800

    refactor pingPong function to include countUp function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,30 @@
-function countUp(number) {
+// function countUp(number) {
+//   var numberArray = [];
+//   for (var i = 1; i <= number; i++) {
+//     numberArray.push(i);
+//   }
+//   return numberArray;
+// };
+//
+// var pingPong = function(number) {
+//   if (number % 3 === 0 && number % 5 === 0) {
+//     return "pingpong";
+//   }
+//   else if (number % 5 === 0) {
+//     return "pong";
+//   }
+//   else if (number % 3 === 0) {
+//     return "ping";
+//   }
+//   else if (number % 3 !== 0 || number % 5 !== 0 || number % 15 !== 0 ) {
+//     return number;
+//   }
+// };
+
+var pingPong = function (number) {
   var numberArray = [];
   for (var i = 1; i <= number; i++) {
     numberArray.push(i);
   }
   return numberArray;
-};
-
-var pingPong = function(number) {
-  if (number % 3 === 0 && number % 5 === 0) {
-    return "pingpong";
-  }
-  else if (number % 5 === 0) {
-    return "pong";
-  }
-  else if (number % 3 === 0) {
-    return "ping";
-  }
-  else if (number % 3 !== 0 || number % 5 !== 0 || number % 15 !== 0 ) {
-    return number;
-  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,23 +1,30 @@
-describe("countUp", function(){
-  it("will count up to a user's number by 1 starting at 1 and put the numbers into an array", function(){
-    expect(countUp(2)).to.eql([1,2]);
-  });
-});
+// describe("countUp", function() {
+//   it("will count up to a user's number by 1 starting at 1 and put the numbers into an array", function() {
+//     expect(countUp(2)).to.eql([1,2]);
+//   });
+// });
+//
+// describe("pingPong", function() {
+//   it("will return ping for numbers divisible by 3", function() {
+//     expect(pingPong(3)).to.equal("ping");
+//   });
+//
+//   it("will return pong for numbers divisible by 5", function() {
+//     expect(pingPong(5)).to.equal("pong");
+//   });
+//
+//   it("will return pingpong for numbers divisible by 3 and 5", function() {
+//     expect(pingPong(15)).to.equal("pingpong");
+//   });
+//
+//   it("will return the number for a number that is not divisible by 3, 5, or 15", function(){
+//     expect(pingPong(1)).to.equal(1);
+//   });
+// });
+
 
 describe("pingPong", function() {
-  it("will return ping for numbers divisible by 3", function() {
-    expect(pingPong(3)).to.equal("ping");
-  });
-
-  it("will return pong for numbers divisible by 5", function() {
-    expect(pingPong(5)).to.equal("pong");
-  });
-
-  it("will return pingpong for numbers divisible by 3 and 5", function() {
-    expect(pingPong(15)).to.equal("pingpong");
-  });
-
-  it("will return the number for a number that is not divisible by 3, 5, or 15", function(){
-    expect(pingPong(1)).to.equal(1);
+  it("will count up to a user's numb er by 1 and put the numbers into an array", function () {
+    expect(pingPong(2)).to.eql([1,2]);
   });
 });
